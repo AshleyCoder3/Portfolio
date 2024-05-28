8c7b5a8431f455cc9324de6d0592996c3b1737c9
ashley burns <burns.ashley331@gmail.com>
2024-05-28 12:15:20 -0400

    Add page links to footer, Add home page layout,

--- src/app/navigation/footer.js
+++ src/app/navigation/footer.js
@@ -6,12 +6,20 @@ Centered
     copyright year by ME | sitemap (??)
  */
 
+import Link from "next/link";
+
 export default function Footer() {
     return (
         <footer className="footer">
             <div className="footer-content">
+                <div>
+                    <Link className="nav-link" href="/">Home</Link>
+                    <Link className="nav-link" href="#">About</Link>
+                    <Link className="nav-link" href="#">Contact</Link>
+                    <Link className="nav-link" href="#">Work</Link>
+                </div>
                 <a className="email" href="#">burns.ashley331@gmail.com</a>
-                <div className="nav-label">
+                <div>
                     <a className="nav-link" href="#">LinkedIN</a>
                     <a className="nav-link" href="#">FaceBook</a>
                     <a className="nav-link" href="#">GitHUB</a>
--- src/app/page.js
+++ src/app/page.js
@@ -17,6 +17,16 @@
 export default function Home() {
     return (
         <div>
+            <div className="welcome">
+                <h3> Welcome to my portfolio. </h3>
+                <div>From experimental passion projects to commissioned work for clients.
+                    I thrive on creative challenges
+                    and enjoy building strong relationships along the way.
+                    Explore my work below, and contact me directly to learn more.
+                </div>
+                <button> Get in Touch</button>
+            </div>
+            <div className="home-projects"> IMAGES</div>
         </div>
     );
 }
